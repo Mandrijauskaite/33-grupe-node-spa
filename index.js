7ebbdb3c3781169e657605308e74c9d17521eb20
Mandrijauskaite <97032997+Mandrijauskaite@users.noreply.github.com>
2022-04-25 16:20:12 +0300

    initial server

---
+++ index.js
@@ -0,0 +1,14 @@
+import { server } from "./lib/server.js";
+
+const app = {};
+
+app.init = () => {
+    // pairuosti pradinius folderoius
+    // pasiruo6ti pradinius failus
+    // prisijungti prie DB
+    // pasileisti serverio logika (jei duoti prieiga prie DB)
+    console.log('pasileidinejame serveri...');
+    // reguliariu procesu paleidimas
+}
+app.onit();
+export { app };
